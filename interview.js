2b4da0a23125ae7e840abd306d75f2975afcbebc
tyagi917 <tyagipranav000@gmail.com>
2021-01-16 14:57:19 +0530

    First commit

---
+++ interview.js
@@ -0,0 +1,39 @@
+function sum()
+{
+	var a=5;
+	function print()
+	{
+	console.log(a);
+
+	}
+	return print;
+}
+var s=sum();
+s();
+
+//assume serach on the basic of name;
+state
+{
+	name:""
+}
+//then take input from serach bar assume we will take name and serach on the basic of name.
+data=[
+{id:1,
+name:"pranav"
+},
+{
+id:2,
+name:"kartik"
+}
+	
+}]
+ var res=data.filter(data=>
+{
+	if(data.name==name)
+	return data
+}
+then apply map on the res and show the data realted to seracing.
+
+
+	
+})
